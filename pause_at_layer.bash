0d1f3487852d55f7fb38c2993b189d40856d01d0
bmiller08 <bmiller08@users.noreply.github.com>
2017-04-22 15:34:42 -0400

    Fix for extruder position setting
    - Remove return z_return and just set correct height on return
    - Remove extruder position

--- pause_at_layer.bash
+++ pause_at_layer.bash
@@ -45,15 +45,12 @@ fi
 
 # Get the target layer position
 z_pos=$( echo "${1} * ${2}" | bc )
-# Set the return to height
-z_return=$( echo "${z_pos} - ${2}" | bc )
 # Get the position of insertion in the code
 gcode_pos=$( grep "; layer ${1}," ${4} )
 
-# Remove extruder position line
-#sed -i "/layer ${1},/b;n;n;n;c ; Remove extruder position line" ${4}
 # Make insertion
-sed -i "s/${gcode_pos}/${gcode_pos}\n; FILAMENT CHANGE added by Pause At Layer\nG28 X Y\nG1 Z${3}\nM0\nG1 Z${z_return}/" ${4}
+sed -e "/G1 Z${z_pos}/,/G1 E0.0000/d" ${4}
+sed -i "s/${gcode_pos}/${gcode_pos}\n; FILAMENT CHANGE added by Pause At Layer\nG28 X Y\nG1 Z${3}\nM0\nG1 Z${z_pos}\n; END FILAMENT CHANGE/" ${4}
 
 # Show location of inserted code
 grep -C 6 "; layer ${1}" ${4}
